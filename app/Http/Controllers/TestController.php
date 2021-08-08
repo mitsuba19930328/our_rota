cd47dc46ab10c818c3699e1a3843de334d88ad22
島崎塁 <mitsuba19930328@gmail.com>
2021-08-09 04:29:47 +0900

    composer導入＆カレンダープロトタイプ作成

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class TestController extends Controller
 {
-  function index()
+  public function index()
   {
-    return view('test');
+    $year = 2021;
+    $month = 8;
+
+    $dateStr = sprintf('%04d-%02d-01', $year, $month);
+    $date = new Carbon($dateStr);
+    // カレンダーを四角形にするため、前月となる左上の隙間用のデータを入れるためずらす
+    $date->subDay($date->dayOfWeek);
+    // 同上。右下の隙間のための計算。
+    $count = 31 + $date->dayOfWeek;
+    $count = ceil($count / 7) * 7;
+    $dates = [];
+
+    for ($i = 0; $i < $count; $i++, $date->addDay()) {
+      // copyしないと全部同じオブジェクトを入れてしまうことになる
+      $dates[] = $date->copy();
+    }
+    return $dates;
   }
 }
